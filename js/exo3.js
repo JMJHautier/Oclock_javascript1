37728b04dbe9e47cba64ef52291ca03a198e5f52
jMoirenc <julien.moirenc@oclock.io>
2021-08-11 14:41:40 +0200

    exo 3

--- js/exo3.js
+++ js/exo3.js
@@ -17,3 +17,34 @@ Notions
 - JS : for ou forEach / setTimeout
 
 */
+
+const boxes = document.querySelectorAll('.box');
+console.log(boxes);
+
+/*
+boxes.forEach(
+    (box, index) => {
+        setTimeout(
+            () => {
+                box.style.display = 'block';
+            },
+            (index + 1) * 1000,
+        );
+    }
+);
+*/
+
+// on peut aussi faire la même chose avec une boucle for
+// et un incrément de boucle déclarée en let
+// mais attention, ce genre de traitement ne fonctionne pas avec 
+// une variable déclarée en var
+for (let index = 0; index < boxes.length; index++){
+    setTimeout(
+        () => {
+            boxes[index].style.display = 'block';
+        },
+        (index + 1) * 1000,
+    );
+}
+
+
