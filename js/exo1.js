6a56cb6c8ba19cabc8159b82436953c68b685892
jMoirenc <julien.moirenc@oclock.io>
2021-08-11 12:12:48 +0200

    exo 1

--- js/exo1.js
+++ js/exo1.js
@@ -18,3 +18,19 @@ Notions
 */
 
 const items = ['Sunglasses', 'Suit', 'Business card', 'Jet ski'];
+
+// on stocke un référene à l'élément du dom dans 
+// lequel on va attacher nos li
+const resultElement = document.getElementById('result');
+
+items.forEach(
+    (item) => {
+        // on crée un nouveau li
+        const liElement = document.createElement('li');
+        // on positionne son contenu textuel
+        liElement.textContent = item;
+        // on accroche le li fraîchement créé dans le dom en tant qu'enfant de resultElement
+        resultElement.appendChild(liElement);
+    }
+);
+
